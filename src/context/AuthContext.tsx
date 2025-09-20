a6118b61a9084e993c90d83ddbcd3a1a72c866cb
Miguel Figuera <36214292+miguelfiguera@users.noreply.github.com>
2025-09-20 02:52:10 -0400

    build error fixed

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useState, ReactNode } from 'react';
+import React, { createContext, useState } from 'react';
+import type { ReactNode } from 'react';
 import { validateCredentials } from '../helpers/cuentasAutorizadas';
 
 export interface AuthContextType {
